213818e09d3935ee6cb86e3c50b55d8288aff727
SabrinaJewson <sejewson@gmail.com>
2020-10-19 06:46:58 +0100

    Support running on Tokio 0.3

--- src/lib.rs
+++ src/lib.rs
@@ -42,6 +42,7 @@ use reqwest::{header, RequestBuilder, Url};
 use serde::de::DeserializeOwned;
 use serde::Deserialize;
 use tokio::sync::{Mutex, MutexGuard};
+use tokio_compat_02::FutureExt;
 
 pub use authorization_url::*;
 pub use endpoints::*;
@@ -127,7 +128,7 @@ impl Client {
             dbg!(&request, body_str(&request));
         }
 
-        let response = self.client.execute(request).await?;
+        let response = self.client.execute(request).compat().await?;
         let status = response.status();
         let text = response.text().await?;
         if !status.is_success() {
@@ -216,7 +217,7 @@ impl Client {
         }
 
         let response = loop {
-            let response = self.client.execute(request.try_clone().unwrap()).await?;
+            let response = self.client.execute(request.try_clone().unwrap()).compat().await?;
             if response.status() != 429 {
                 break response;
             }
